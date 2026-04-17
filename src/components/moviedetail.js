ca24bc44917b47e3077d65592304a8a9e4553765
roylin115 <roylin115@gmail.com>
2026-04-17 05:37:27 -0600

    .

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchMovie } from '../actions/movieActions';
 import { useDispatch, useSelector } from 'react-redux';
 import { Card, ListGroup, ListGroupItem, Image } from 'react-bootstrap';
@@ -11,60 +11,117 @@ const MovieDetail = () => {
   const selectedMovie = useSelector(state => state.movie.selectedMovie);
   const loading = useSelector(state => state.movie.loading); // Assuming you have a loading state in your reducer
   const error = useSelector(state => state.movie.error); // Assuming you have an error state in your reducer
+  const [rating, setRating] = useState('');
+  const [reviewText, setReviewText] = useState('');
+  
 
 
   useEffect(() => {
     dispatch(fetchMovie(movieId));
   }, [dispatch, movieId]);
 
-  const DetailInfo = () => {
-    if (loading) {
-      return <div>Loading....</div>;
-    }
+  const submitReview = async () => {
+    try {
+      const token = localStorage.getItem("token");
 
-    if (error) {
-      return <div>Error: {error}</div>;
-    }
+      const response = await fetch("http://localhost:8080/reviews", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": token
+        },
+        body: JSON.stringify({
+          movieId: movieId,
+          rating: Number(rating),
+          review: reviewText
+        })
+      });
 
-    if (!selectedMovie) {
-      return <div>No movie data available.</div>;
-    }
+      const data = await response.json();
 
-    return (
-      <Card className="bg-dark text-dark p-4 rounded">
-        <Card.Header>Movie Detail</Card.Header>
-        <Card.Body>
-          <Image className="image" src={selectedMovie.imageUrl} thumbnail />
-        </Card.Body>
-        <ListGroup>
-          <ListGroupItem>{selectedMovie.title}</ListGroupItem>
-          <ListGroupItem>
-            {selectedMovie.actors.map((actor, i) => (
-              <p key={i}>
-                <b>{actor.actorName}</b> {actor.characterName}
-              </p>
-            ))}
-          </ListGroupItem>
-          <ListGroupItem>
-            <h4>
-              <BsStarFill /> {selectedMovie.avgRating}
-            </h4>
-          </ListGroupItem>
-        </ListGroup>
-        <Card.Body className="card-body bg-white">
-          {selectedMovie.reviews.map((review, i) => (
-            <p key={i}>
-              <b>{review.username}</b>&nbsp; {review.review} &nbsp; <BsStarFill />{' '}
-              {review.rating}
-            </p>
-          ))}
-        </Card.Body>
-      </Card>
-    );
+      if (!response.ok) {
+        throw new Error(data.message || "Failed to submit review");
+      }
+
+      window.location.reload(); // refresh page after submit
+    } catch (err) {
+      console.error("Error submitting review:", err);
+    }
   };
 
-  return <DetailInfo />;
+
+
+  if (loading) {
+    return <div>Loading....</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!selectedMovie) {
+    return <div>No movie data available.</div>;
+  }
+
+  return (
+    <Card className="bg-dark text-dark p-4 rounded">
+      <Card.Header>Movie Detail</Card.Header>
+      <Card.Body className="card-body bg-white">
+
+
+      {/* ADD REVIEW FORM */}
+      <div style={{ marginTop: "20px" }}>
+        <h5>Add Review</h5>
+
+        <input
+          type="number"
+          min="1"
+          max="5"
+          placeholder="Rating (1-5)"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <br />
+
+        <textarea
+          placeholder="Write your review"
+          value={reviewText}
+          onChange={(e) => setReviewText(e.target.value)}
+        />
+
+        <br />
+
+        <button onClick={submitReview}>Submit</button>
+      </div>
+    </Card.Body>
+      <ListGroup>
+        <ListGroupItem>{selectedMovie.title}</ListGroupItem>
+        <ListGroupItem>
+          {selectedMovie.actors.map((actor, i) => (
+            <p key={i}>
+              <b>{actor.actorName}</b> {actor.characterName}
+            </p>
+          ))}
+        </ListGroupItem>
+        <ListGroupItem>
+          <h4>
+            <BsStarFill /> {selectedMovie.avgRating}
+          </h4>
+        </ListGroupItem>
+      </ListGroup>
+      <Card.Body className="card-body bg-white">
+        {selectedMovie.movieReviews.map((review, i) => (
+          <p key={i}>
+            <b>{review.username}</b>&nbsp; {review.review} &nbsp; {' '}
+            {review.rating}
+          </p>
+        ))}
+      </Card.Body>
+    </Card>
+  );
 };
+
 
 
 export default MovieDetail;
--- src/components/movielist.js
+++ src/components/movielist.js
@@ -1,56 +1,112 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMovies, setMovie } from "../actions/movieActions";
 import { Link } from 'react-router-dom';
-import { Image, Nav, Carousel } from 'react-bootstrap';
+import { Image, Nav, Carousel, Form, Button } from 'react-bootstrap';
 import { BsStarFill } from 'react-icons/bs';
 
 function MovieList() {
     const dispatch = useDispatch();
     const movies = useSelector(state => state.movie.movies);
 
-    // Memoize the movies array
-    const memoizedMovies = useMemo(() => {
-        return movies;
-    }, [movies]);
+    const [searchResults, setSearchResults] = useState([]);
+    const [query, setQuery] = useState('');
+    const [isSearching, setIsSearching] = useState(false);
+
+    const memoizedMovies = useMemo(() => movies, [movies]);
 
     useEffect(() => {
         dispatch(fetchMovies());
     }, [dispatch]);
 
     const handleSelect = (selectedIndex) => {
-        // Use memoizedMovies here
-        dispatch(setMovie(memoizedMovies[selectedIndex]));
+        const list = isSearching ? searchResults : memoizedMovies;
+        dispatch(setMovie(list[selectedIndex]));
     };
 
     const handleClick = (movie) => {
         dispatch(setMovie(movie));
     };
 
-    if (!memoizedMovies) { // Use memoizedMovies here
+    const searchMovies = async () => {
+        try {
+            const token = localStorage.getItem('token');
+
+            const response = await fetch("http://localhost:8080/search", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": token
+                },
+                body: JSON.stringify({ query })
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.message || "Search failed");
+            }
+
+            setSearchResults(data);
+            setIsSearching(true);
+        } catch (err) {
+            console.error("Error searching movies:", err);
+        }
+    };
+
+    const resetSearch = () => {
+        setIsSearching(false);
+        setSearchResults([]);
+        setQuery('');
+    };
+
+    if (!memoizedMovies) {
         return <div>Loading....</div>;
     }
 
+    const displayMovies = isSearching ? searchResults : memoizedMovies;
+
     return (
-        <Carousel onSelect={handleSelect} className="bg-dark text-light p-4 rounded">
-          {memoizedMovies.map((movie) => (
-            <Carousel.Item key={movie._id}>
-              {/* Use Nav.Link with "as={Link}" to avoid nested anchors */}
-              <Nav.Link
-                as={Link}
-                to={`/movie/${movie._id}`}
-                onClick={() => handleClick(movie)}
-              >
-                <Image className="image" src={movie.imageUrl} thumbnail />
-              </Nav.Link>
-              <Carousel.Caption>
-                <h3>{movie.title}</h3>
-                <BsStarFill /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
-              </Carousel.Caption>
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      );
-    }
+        <div>
+
+            {/* 🔍 SEARCH UI */}
+            <div className="mb-3">
+                <Form.Control
+                    type="text"
+                    placeholder="Search movies or actors..."
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <Button className="mt-2 me-2" onClick={searchMovies}>
+                    Search
+                </Button>
+                {isSearching && (
+                    <Button variant="secondary" onClick={resetSearch}>
+                        Reset
+                    </Button>
+                )}
+            </div>
+
+            {/* 🎬 MOVIE DISPLAY */}
+            <Carousel onSelect={handleSelect} className="bg-dark text-light p-4 rounded">
+                {displayMovies.map((movie) => (
+                    <Carousel.Item key={movie._id}>
+                        <Nav.Link
+                            as={Link}
+                            to={`/movie/${movie._id}`}
+                            onClick={() => handleClick(movie)}
+                        >
+                            <Image className="image" src={movie.imageUrl} thumbnail />
+                        </Nav.Link>
+                        <Carousel.Caption>
+                            <h3>{movie.title}</h3>
+                            <BsStarFill /> {movie.avgRating || "N/A"} &nbsp;&nbsp; {movie.releaseDate}
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+        </div>
+    );
+}
 
 export default MovieList;
